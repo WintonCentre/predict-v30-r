d0714baadcdc242fa53ce8c208ce49547ec893e3
gmp26 <gmp26@cam.ac.uk>
2022-11-22 15:58:08 +0000

    c.oth and r.oth added to mi only once

--- R/benefits30.R
+++ R/benefits30.R
@@ -199,6 +199,8 @@ benefits30 <- function(
 
     # Generate cumulative survival non-breast mortality
     # Incorporates the increased mortality associated with chemo and radiotherapy
+    # WINTON Fix: c.oth and r.oth have already been included
+    #s.cum.oth <- exp(-exp(mi + c.oth + r.oth)*base.m.cum.oth)
     s.cum.oth <- exp(-exp(mi + c.oth + r.oth)*base.m.cum.oth)
 
     # Convert cumulative mortality rate into cumulative risk
